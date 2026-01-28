fe0a128912bd535a3844d285dc3d827de957f27c
Biplob Hasan <your-email@example.com>
2026-01-28 21:32:19 +0600

    chore: final package.json config for vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,9 @@
-import "dotenv/config";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
-export { prisma };
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
